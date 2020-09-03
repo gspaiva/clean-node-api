ae5b026e1186fa4769e7056855ada9b12e3884aa
gspaivaequals <gabriel.paiva@equals.com.br>
2020-09-03 20:41:13 -0300

    fix: change regex to find to jest apply ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  roots:['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  roots:["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
    transform: {
-     '.+//.ts$':'ts-jest'
+    "^.+\\.(ts|tsx)$":"ts-jest"
    },
 };
