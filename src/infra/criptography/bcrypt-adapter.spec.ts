56ee086ff6a89d99addbdbb673cc5e17d6ef027e
gspaivaequals <gabriel.paiva@equals.com.br>
2020-09-11 08:04:20 -0300

    refactor: change BCryptAdapter instance to be make in a helper method factory

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -7,17 +7,21 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const salt = 12
+
+const makeSut = (): BCryptAdapter => {
+  return new BCryptAdapter(salt)
+}
+
 describe('BCryptAdapter infra', () => {
   test('BCryptAdapter should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BCryptAdapter(salt)
+    const sut = makeSut()
     const bcryptSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(bcryptSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('BCryptAdapter should return hash on success', async () => {
-    const salt = 12
     const sut = new BCryptAdapter(salt)
     const hash = await sut.encrypt('any_value')
     expect(hash).toEqual('hash')
