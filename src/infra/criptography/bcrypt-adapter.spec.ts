35d55ec99daf6c4f52c070f5ac5e14469b529f0e
gspaivaequals <gabriel.paiva@equals.com.br>
2020-09-11 08:10:26 -0300

    test: ensure BCryptAdapter should throw if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -26,4 +26,11 @@ describe('BCryptAdapter infra', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toEqual('hash')
   })
+
+  test('BCryptAdapter should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
