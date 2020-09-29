22b711a974385be54bf0d279d6feef6428faa169
Gabriel Paiva <gabriel_silvapaiva@hotmail.com>
2020-09-29 19:43:15 -0300

    test: ensure LogControllerDecorator returns the result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -9,10 +9,13 @@ interface sutTypes {
 class ControllerStub implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = {
-      statusCode: 200,
       body: {
-        name: 'Gabriel'
-      }
+        email: 'any_email@mail.com',
+        name: 'any name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      },
+      statusCode: 200
     }
     return await new Promise(resolve => resolve(httpResponse))
   }
@@ -41,5 +44,28 @@ describe('LogControllerDecorator tests', () => {
 
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeController()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      },
+      statusCode: 200
+    })
   })
 })
